bf077150f3c7e1375306d7dcea465e230d8b4163
Aditya Bhuran <adityabhuran240802@gmail.com>
2025-07-04 02:51:49 -0400

    validated input - commit 2

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
 
-
-
-
+const x = document.getElementById('number1');
+const y = document.getElementById('number2');
+const result = document.getElementById('result');
 
 
 
 //make the switch case first
 function calculate(operator) {
+    const num1 = parseFloat(x.value);
+    const num2 = parseFloat(y.value);
+    // Validate input
+    if (isNaN(num1) || isNaN(num2)) {
+        result.innerText = 'Please enter valid numbers.';
+        return;
+    }
     switch (operator) {
         case '+':
+            result.innerText = num1 + num2;
             break;
         case '-':
-            break;      
-        case '*':
+            result.innerText = num1 - num2;
             break;
-        case '/':
+        case '*':
+            result.innerText = num1 * num2;
+            break;
+        case '/': //divide 
+            if (num2 === 0) {
+                result.innerText = 'Division by zero is not allowed.';
+            } else {
+                result.innerText = num1 / num2;
+            }
             break;
         default:
 }
