7cb486f41471968a32bd6bace0b9ee560c61508b
Aditya Bhuran <adityabhuran240802@gmail.com>
2025-07-04 02:45:59 -0400

    switchcase login -initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+
+//make the switch case first
+function calculate(operator) {
+    switch (operator) {
+        case '+':
+            break;
+        case '-':
+            break;      
+        case '*':
+            break;
+        case '/':
+            break;
+        default:
+}
+
+}
